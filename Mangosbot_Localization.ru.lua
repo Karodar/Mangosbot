bf662285dc1562d0791c511ad5ab72f267e96e27
Karodar <karodar@mail.ru>
2025-07-08 16:13:58 +0300

    feat: init

---
+++ Mangosbot_Localization.ru.lua
@@ -0,0 +1,11 @@
+
+BINDING_HEADER_MANGOSBOT = "Mangosbot";
+BINDING_NAME_ROSTER = "Показать/Скрыть список ботов";
+BINDING_NAME_FOLLOW = "Движение бота: Следовать за хозяином";
+BINDING_NAME_STAY = "Движение бота: Стоять";
+BINDING_NAME_PASSIVE = "Движение бота: Пассивный режим";
+BINDING_NAME_FLEE = "Движение бота: Бежать";
+BINDING_NAME_LOOT = "Действия бота: Собирать добычу";
+BINDING_NAME_ATTACK = "Действия бота: Атаковать";
+BINDING_NAME_PULL = "Действия бота: Притянуть";
+BINDING_NAME_BOOST = "Действия бота: Включить/выключить ускорение";
